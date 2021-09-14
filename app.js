0d8a257ffa3d11b419fb672aef7dff527acf0b5e
Ravi02205 <pravi00512@gmail.com>
2021-09-15 01:46:38 +0530

    Canvas Properties

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-
-
 const Application = PIXI.Application
 
 const app = new Application({
+  // settting up the height and width of canvas
+  width: 500,
+  height: 500,
+  //   will make the bg color of canvas transparent
+  transparent: false,
+  // will fix the pixels of canvas
+  antialise: true,
+})
 
-});
+// app.renderer ==> is element is used to draw element in canvas
+// app.view ==> canvas itself
 
-document.body.appendChild(app.view);
+// change the background color of canvas
+app.renderer.backgroundColor = 0x23395d
+
+// We can use resize method to resize the canvas
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+// to remove the default margin and padding we will set the position absolute in canvas
+app.renderer.view.style.position='absolute';
+
+document.body.appendChild(app.view)
