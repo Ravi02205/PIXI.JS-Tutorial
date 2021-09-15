d84de5ff0c20c6f045b998283fb4278cc1b1538f
Ravi02205 <pravi00512@gmail.com>
2021-09-15 17:07:37 +0530

    Adding and creating Shapes

--- app.js
+++ app.js
@@ -23,3 +23,101 @@ app.renderer.resize(window.innerWidth, window.innerHeight);
 app.renderer.view.style.position='absolute';
 
 document.body.appendChild(app.view)
+
+
+// creating the shapes
+// every shape is included in the PIXI.Graphics()
+
+const Graphics = PIXI.Graphics;
+
+// creating the rectangle
+const rectangle = new Graphics();
+
+//creating rectangle we need to follow set of instructions
+// rectangle.beginFill(0xAA33BB);
+// tell the graphics to draw this as rectangle
+// rectangle.drawRect(x-cordinate,Y-cordinate,width,height);
+// rectangle.drawRect(200,200,100,120);
+// call the endfill method
+// rectangle.endFill();
+
+// the above methods are chain-able 
+rectangle.beginFill(0xaa33bb)
+.lineStyle(4,0xffea00)
+.drawRect(200, 200, 100, 120)
+.endFill();
+
+// by using linestyle we can add the line stroke
+
+// to add tis rectangle to canvas we use 
+app.stage.addChild(rectangle);
+
+
+// creating the Polygon
+const poly = new Graphics();
+poly
+  .beginFill(0xaa33bb)
+  .lineStyle(4, 0xffea00,1)
+  .drawPolygon([
+    600,50,
+    800,150,
+    900,300,
+    400,400
+  ])
+  .endFill()
+
+
+  //   .drawPolygon([ 
+//     x-cordinate,y-cordinate,
+//     x-cordinate,y-cordinate,        
+//     x-cordinate,y-cordinate
+// ])
+
+
+// by using linestyle we can add the line stroke
+
+// to add tis polygon to canvas we use 
+app.stage.addChild(poly);
+
+
+// creating cricle 
+const circle= new Graphics();
+circle
+  .beginFill(0x22aacc)
+  .drawCircle(400,200,80)
+  .endFill();
+// to add this circle to canvas we use 
+app.stage.addChild(circle);
+
+
+// creating lie
+const line=new Graphics();
+line.lineStyle(5,0xffea00,1)
+.moveTo(1500,100)
+.lineTo(1500,800);
+// moveto=> for moving the line (x-c0,y-co)
+// lineto=> for ending point the line (x-c0,y-co)
+app.stage.addChild(line);
+
+// TO ADD EXTRA SHAPES LIKE EC-LIPS AND ROUNDED RECTANGLE WE USE THE graphics-EXTRA module
+// creating torus(just like doughnut)
+const torus= new Graphics();
+torus
+.beginFill(0xfffddd)
+.drawTorus(100,700,80,100,0,Math.PI/2)
+.endFill();
+
+// .drawTorus(x-cor of center of torus,y-cor of center of torus,radius of inner circle,radius of outer circle)
+
+app.stage.addChild(torus);
+
+
+// creating the star
+const star=new Graphics();
+
+star
+.beginFill(0xADADAD)
+.drawStar(900,700,300,80)
+.endFill();
+
+app.stage.addChild(star);
